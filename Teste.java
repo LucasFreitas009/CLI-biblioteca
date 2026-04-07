71a2fdaa6b106e11641abb919e85a70cfed8b650
LucasFreitas009 <genicefreitas64@gmail.com>
2026-04-07 17:42:52 -0300

    CLIInterface necessita de melhoria

--- Teste.java
+++ Teste.java
@@ -5,25 +5,23 @@ import java.util.List;
 
 import main.java.connections.Connect;
 import main.java.dao.ClientesDAO;
+import main.java.interfaces.CLIInterface;
 import main.java.model.Cliente;
 
 //import main.java.connections.Connect;
 
 public class Teste {
     public static void main(String[] args) {
-
+        
         try (
             Connection connection = Connect.connect();
         ) {
             List<Cliente> clientes = ClientesDAO.selectClientes(connection);
-
-            for(int i = 0; i < clientes.size(); i ++) {
-                System.out.println("id: " + clientes.get(i).getId());
-                System.out.println("nome do cliente: " + clientes.get(i).getNome());
-                System.out.println("endereço: " + clientes.get(i).getEndereco());
-                System.out.println("-".repeat(15));
-
-            }
+            List<String> columns = ClientesDAO.getColumns();
+            
+            CLIInterface cliInterface = new CLIInterface();
+            
+            cliInterface.all(columns, clientes);
 
         } catch (SQLException e) {
             System.out.println("Error");
--- src/main/java/interfaces/CLIInterface.java
+++ src/main/java/interfaces/CLIInterface.java
@@ -1,6 +1,29 @@
 package main.java.interfaces;
 
-public class CLIInterface {
-    
-    public void all() {}
+import java.util.List;
+
+import main.java.model.Cliente;
+
+public class CLIInterface  {
+
+    public void all(List<String> columns, List<Cliente> clientes) {
+        System.out.println("============ clientes ============");
+        
+        for(int i = 0; i < clientes.size(); i++) {
+
+            System.out.printf("""
+                _____________________________________
+                |    %s      |   %s     |    %s     |
+                |____________|__________|___________|       
+                |    %d      |    %s    |    %s     | 
+                |____________|__________|___________|\n
+                """, columns.get(0),
+                    columns.get(1),
+                    columns.get(2),
+                    clientes.get(i).getId(),
+                    clientes.get(i).getNome(),
+                    clientes.get(i).getEndereco()
+                );
+        }
+    }
 }
